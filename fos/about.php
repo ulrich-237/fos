5410dbd4e7f4aa9799e6c48a4b49f7ead159cd8c
ulrich-237 <jordifokou@gmail.com>
2023-01-12 16:02:20 +0100

    initial commit

---
+++ fos/about.php
@@ -0,0 +1,19 @@
+ <!-- Masthead-->
+        <header class="masthead">
+            <div class="container h-100">
+                <div class="row h-100 align-items-center justify-content-center text-center">
+                    <div class="col-lg-10 align-self-center mb-4 page-title">
+                    	<h1 class="text-white">About Us</h1>
+                        <hr class="divider my-4 bg-dark" />
+                    </div>
+                    
+                </div>
+            </div>
+        </header>
+
+    <section class="page-section">
+        <div class="container">
+    <?php echo html_entity_decode($_SESSION['setting_about_content']) ?>        
+            
+        </div>
+        </section>
